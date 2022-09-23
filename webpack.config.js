77d71a85b1505a80891ef4e6df1b5b809f4250e9
smit.patel <smit@cmsminds.com>
2022-09-23 18:34:10 +0530

    Login Page Design

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,59 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
 module.exports = {
-    context: __dirname,
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: "/"
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jp?g|svg|gif)$/,
-                use: [{
-                    loader: "file-loader"
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'),
-            filename: 'index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development')
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: "bundle.css",
-        })
-    ]
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + '/dist/bundle/',
+		publicPath: '/static/'
+	},
+	devServer: {
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				use: { loader: 'babel-loader' },
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: MiniCssExtractPlugin.loader },
+					{
+						loader: 'css-loader',
+					},
+				]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+			filename: 'index.html'
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('development')
+			}
+		}),
+		new MiniCssExtractPlugin({
+			filename: "bundle.css",
+		})
+	]
 };

28c85cc9ceb521ed9f02e83a809b8eb275796742
smit.patel <smit@cmsminds.com>
2022-09-23 17:59:42 +0530

    Login Page Design

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,47 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const path = require( 'path' );
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
-	context: __dirname,
-	entry: './src/index.js',
-	output: {
-		path: path.resolve( __dirname, 'dist' ),
-		filename: 'main.js',
-		publicPath: "/"
-	},
-	devServer: {
-		historyApiFallback: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|jp?g|svg|gif)$/,
-				use: [{
-					loader: "file-loader"
-				}]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: path.resolve( __dirname, 'public/index.html' ),
-			filename: 'index.html'
-		})
-	]
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: "/"
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jp?g|svg|gif)$/,
+                use: [{
+                    loader: "file-loader"
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        })
+    ]
 };

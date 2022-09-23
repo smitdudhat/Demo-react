66af404a49bf85aa5cdfbc0f1a812ccfb67d3abb
smit.patel <smit@cmsminds.com>
2022-09-23 12:12:17 +0530

    Package.json

--- server.js
+++ server.js
@@ -126,4 +126,6 @@ app.get('/post/:id', (req, res) => {
 });
 
 
-app.listen( process.env.PORT || 5000, () => console.log( 'Listening on port 5000' ) );
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
